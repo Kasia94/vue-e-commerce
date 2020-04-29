1c24fb5e6c24e3aa64be4fd30145809bf0d833a0
qlash <33455949+qlash@users.noreply.github.com>
2020-04-29 15:25:08 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
+    "plugin:vue/strongly-recommended",
+    "plugin:vue/recommended",
     '@vue/standard'
   ],
   parserOptions: {
