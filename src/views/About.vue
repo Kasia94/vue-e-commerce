f78cc811f57eb38491d77e5924e8c43e30fcd112
Katarzyna <katarzyna.skibinska94@gmail.com>
2020-09-01 11:50:01 +0200

    fix

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about">
-    <h2>
+    <h4>
       „Autentyzm, szczerość, pozytywna energia w bu­­dowaniu firmy to wartości, które od samego początku stanowią o naszym charakterze oraz całej naszej działalności”.
-    </h2>
+    </h4>
   </div>
 </template>
 <style lang="scss">
